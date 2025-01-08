863c2d6d19fe459d75391f77a8ec0118c1d9dd36
Mohd Yasir <yasirkn26@gmail.com>
2025-01-08 22:26:30 +0530

    Schedule Changes

---
+++ src/conferenceTrack/conferenceTrack.jsx
@@ -0,0 +1,55 @@
+import "./conferenceTrack.css"
+export default function ConfernceTrack() {
+    const topics = [
+        "Artificial Intelligence",
+        "Machine Learning and Data Science",
+
+        "Cybersecurity and Network Technologies",
+
+        "Internet of Things(IoT)",
+
+        "Blockchain",
+
+        "Technologies and Smart Systems",
+
+        "Underwater GPS and Laser Communication",
+
+        "VLSI and Embedded Systems",
+
+        "Image Processing, Signal Processing and Communication Systems",
+
+        "Biomedical Engineering and Healthcare Technologies",
+
+        "Power Electronics",
+
+        "Power Systems and Renewable Energy Mechatronics",
+
+        "Robotics",
+
+        "Control and Automation",
+
+        "Electric & Hybrid Vehicles and Battery Technologies"
+    ];
+    return (
+        <>
+            <div className="conference">
+                <h2>Conference Tracks : </h2>
+                <div className="conference-track">
+                    <ul>
+                        {topics.map((topic, index) => (
+                            <li key={index}>{topic}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+        </>
+    )
+}
